e48a2ffdb79b56d44a25b6da1f2e1815d29298ca
wangxi3 <wangxi3@huya.com>
2020-08-23 16:53:40 +0800

    返回值解析修复

--- src/main/java/com/tencent/tars/tup/ServantInvokeContext.java
+++ src/main/java/com/tencent/tars/tup/ServantInvokeContext.java
@@ -6,78 +6,86 @@ import com.tencent.tars.protocol.JsonConst;
 import java.util.Map;
 
 public class ServantInvokeContext {
-    private Object[] arguments;
-    private Object[] retArguments = null;
-    private Object retVal;
-    private JsonObject jsonRetVal;
-    private Map<String,String> status;
-    private long sendBytes;
-
-
-    public ServantInvokeContext(JsonObject retVal, JsonObject[] arguments) {
-        this.arguments = arguments;
-        this.jsonRetVal = retVal;
-    }
-
-    public ServantInvokeContext(Object retVal, Object[] arguments) {
-        this.arguments = arguments;
-        this.retVal = retVal;
-    }
-
-    public long getSendBytes() {
-        return sendBytes;
-    }
-
-    public void setSendBytes(long sendBytes) {
-        this.sendBytes = sendBytes;
-    }
-
-    public void setStatus(Map<String, String> status) {
-        this.status = status;
-    }
-
-    public Map<String, String> getStatus() {
-        return status;
-    }
-
-    public Object[] getArgumentValues() {
-        return arguments;
-    }
-
-    public Object getRetValue() {
-        return retVal;
-    }
-
-    public void setArguments(Object[] arguments) {
-        this.arguments = arguments;
-    }
-
-    public void setRetVal(Object retVal) {
-        this.retVal = retVal;
-    }
-
-    public void setJsonRetVal(JsonObject retVal) {
-        this.jsonRetVal = retVal;
-    }
-
-    public JsonObject getJsonRetVal() {
-        if (this.jsonRetVal == null) {
-            this.jsonRetVal = new JsonObject();
-            this.jsonRetVal.addProperty(JsonConst.KEY_TYPE, JsonConst.VOID);
-            this.jsonRetVal.addProperty(JsonConst.KEY_TAG, 0);
-            this.jsonRetVal.addProperty(JsonConst.KEY_VALUE, "null");
-        }
-        if (this.jsonRetVal.get(JsonConst.KEY_TAG) == null) {
-            this.jsonRetVal.addProperty(JsonConst.KEY_TAG, 0);
-        }
-        return this.jsonRetVal;
-    }
-
-    public Object[] getRetArguments() {
-        return retArguments;
-    }
-
-    public void setRetArguments(Object[] retArguments) {
-        this.retArguments = retArguments;
-    }
+	private Object[] arguments;
+	private Object[] retArguments = null;
+	private Object retVal;
+	private JsonObject jsonRetVal;
+	private Map<String, String> status;
+	private long sendBytes;
+	
+	
+	public ServantInvokeContext(JsonObject retVal, JsonObject[] arguments) {
+		this.arguments = arguments;
+		this.jsonRetVal = retVal;
+	}
+	
+	public ServantInvokeContext(Object retVal, Object[] arguments) {
+		this.arguments = arguments;
+		this.retVal = retVal;
+	}
+	
+	public long getSendBytes() {
+		return sendBytes;
+	}
+	
+	public void setSendBytes(long sendBytes) {
+		this.sendBytes = sendBytes;
+	}
+	
+	public Map<String, String> getStatus() {
+		return status;
+	}
+	
+	public void setStatus(Map<String, String> status) {
+		this.status = status;
+	}
+	
+	public Object[] getArgumentValues() {
+		return arguments;
+	}
+	
+	public Object getRetValue() {
+		return retVal;
+	}
+	
+	public void setArguments(Object[] arguments) {
+		this.arguments = arguments;
+	}
+	
+	public void setRetVal(Object retVal) {
+		this.retVal = retVal;
+	}
+	
+	public JsonObject getJsonRetVal() {
+		if (this.jsonRetVal == null) {
+			this.jsonRetVal = new JsonObject();
+			this.jsonRetVal.addProperty(JsonConst.KEY_TYPE, JsonConst.VOID);
+			this.jsonRetVal.addProperty(JsonConst.KEY_TAG, 0);
+			this.jsonRetVal.addProperty(JsonConst.KEY_VALUE, "null");
+		}
+		//returnType is tars
+		if (this.jsonRetVal.get(JsonConst.KEY_TAG) == null && this.jsonRetVal.get(JsonConst.KEY_TYPE) == null && this.jsonRetVal.get(JsonConst.KEY_VALUE) == null) {
+			JsonObject ret = new JsonObject();
+			ret.addProperty(JsonConst.KEY_TAG, 0);
+			ret.addProperty(JsonConst.KEY_TYPE, JsonConst.TARS);
+			ret.add(JsonConst.KEY_VALUE, this.jsonRetVal);
+			return ret;
+		}
+		if (this.jsonRetVal.get(JsonConst.KEY_TAG) == null) {
+			this.jsonRetVal.addProperty(JsonConst.KEY_TAG, 0);
+		}
+		return this.jsonRetVal;
+	}
+	
+	public void setJsonRetVal(JsonObject retVal) {
+		this.jsonRetVal = retVal;
+	}
+	
+	public Object[] getRetArguments() {
+		return retArguments;
+	}
+	
+	public void setRetArguments(Object[] retArguments) {
+		this.retArguments = retArguments;
+	}
 }
